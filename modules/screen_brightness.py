dbe624f6f25e7833115582a161d5201aa0d3cae4
derTim <mail@tim-jordan.de>
2022-10-22 19:51:15 +0200

    add brightness controll via ddcutil

---
+++ modules/screen_brightness.py
@@ -0,0 +1,33 @@
+import subprocess
+
+
+class Addon():
+    name = 'Screen Brightness'
+    icon = 'mdi:monitor'
+    unit = '%'
+    sensor_type = 'sensor'
+
+    def exposedControls(self):
+        return {
+            "Screen_Brightness": {
+                "type": "number",
+                "icon": "mdi:monitor",
+                "min": 0,
+                "max": 100,
+            }
+        }
+
+    def getInfo(self):
+        stdout = subprocess.run(
+            "ddcutil getvcp 10 --bus 21'",
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE).stdout.decode("UTF-8")
+        results = stdout.lower()
+
+        status = results.strip().replace('VCP code 0x10 (Brightness                    ): current value =   ', '').replace(', max value =   100','').strip()
+        print(status)
+        return status
+
+    def startControl(self, topic, data):
+        subprocess.call('ddcutil setvcp 10 ' + data + ' --bus 21', shell=True)
